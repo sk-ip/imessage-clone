91741ca26a860bfb28b8f53c60abd6343a710fb2
Sudhanshu kumar <sudhanshukumar5459@gmail.com>
2021-06-23 07:51:02 +0000

    Add Sidebar, SidebarChat and Sidebar Component

---
+++ src/Imessage.js
@@ -0,0 +1,15 @@
+import React from "react";
+import "./Imessage.css";
+import Sidebar from "./Sidebar";
+
+function Imessage() {
+  return (
+    <div className="imessage">
+      {/* Sidebar */}
+      <Sidebar />
+      {/* Chatbar */}
+    </div>
+  );
+}
+
+export default Imessage;
